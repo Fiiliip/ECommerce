f57787ab17686de776860c40b16eaf08050c14c3
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2023-10-23 23:28:32 +0200

    [Storefront] Added category card.

---
+++ app/src/plugins/app@ecommerce/storefront/_components/a-category-card.vue
@@ -0,0 +1,29 @@
+<template>
+    <button @click="null" class="flex py-1 px-3 justify-between border rounded-md hover:bg-zinc-200">
+        <img src="">
+        <p class="">{{ title }}</p>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        img: {
+            type: String,
+            default: '',
+        },
+        title: {
+            type: String,
+            default: '',
+        },
+    },
+    data() {
+        return {
+            //
+        }
+    },
+    methods: {
+        //
+    }
+}
+</script>
