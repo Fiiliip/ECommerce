ce883f3897ac0886be10abcd9c28afcf5b6f44e9
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2023-10-23 23:26:41 +0200

    [Footer] Added footer.

--- app/src/plugins/app/_config/App.vue
+++ app/src/plugins/app/_config/App.vue
@@ -2,6 +2,7 @@
   <div>
     <a-header />
     <RouterView />
+    <a-footer />
   </div>
 </template>
 
---
+++ app/src/plugins/app/_layout/footer/a-footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer class="w-full flex flex-wrap sm:flex-nowrap gap-8 px-5 py-5 sm:justify-evenly text-sm text-white bg-zinc-900">
+        <div>
+            <h2 class="mb-2 font-medium">O nás</h2>
+            <a class="block" href="#">Čo je ECommerce?</a>
+            <a class="block" href="#">Kontakt</a>
+            <a class="block" href="#">Lorem ipsum dolor sit amet</a>
+            <a class="block" href="#">Lorem ipsum dolor sit amet</a>
+        </div>
+
+        <div>
+            <h2 class="mb-2 font-medium">Návody</h2>
+            <a class="block" href="#">Ako bezpečne nakupovať?</a>
+            <a class="block" href="#">Ako vytvoriť úspešný inzerát?</a>
+            <a class="block" href="#">Lorem ipsum dolor</a>
+            <a class="block" href="#">Lorem ipsum dolor</a>
+        </div>
+
+        <div>
+            <h2 class="mb-2 font-medium">Nadpis kategórie</h2>
+            <a class="block" href="#">Čo je ECommerce?</a>
+            <a class="block" href="#">Lorem ipsum dolor sit amet</a>
+            <a class="block" href="#">Lorem ipsum dolor sit amet</a>
+            <a class="block" href="#">Lorem ipsum dolor sit amet</a>
+        </div>
+    </footer>
+</template>
