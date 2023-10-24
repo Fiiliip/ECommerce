8c730022cbeeffabee6fa7b8971b6ae719ba5fd6
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2023-10-24 23:15:45 +0200

    [Router] Added auth guard.

--- app/src/plugins/app/_config/router.ts
+++ app/src/plugins/app/_config/router.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from "@/plugins/app/_config/store"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,22 +12,26 @@ const router = createRouter({
     {
       path: '/auth/login',
       name: 'Login',
-      component: () => import('@/plugins/app@auth/login/login.vue')
+      component: () => import('@/plugins/app@auth/login/login.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/auth/register',
       name: 'Register',
-      component: () => import('@/plugins/app@auth/register/register.vue')
+      component: () => import('@/plugins/app@auth/register/register.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/auth/forgot-password',
       name: 'Forgot Password',
-      component: () => import('@/plugins/app@auth/forgot-password/forgot-password.vue')
+      component: () => import('@/plugins/app@auth/forgot-password/forgot-password.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/auth/reset-password',
       name: 'Reset Password',
-      component: () => import('@/plugins/app@auth/reset-password/reset-password.vue')
+      component: () => import('@/plugins/app@auth/reset-password/reset-password.vue'),
+      meta: { requiresAuth: false },
     },
     {
       path: '/category/:slug',
@@ -39,6 +44,26 @@ const router = createRouter({
       component: () => import('@/plugins/app@ecommerce/product/product.vue')
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+	localStorage.setItem("ecommerce_user_lastSeen", Date())
+
+	if (to.meta.requiresAuth === true) {
+		if (!store.getters["auth/isLoggedIn"]) {
+			return next({ name: "Login" })
+		}
+
+		return next()
+	} else if (to.meta.requiresAuth === false) {
+		if (store.getters["auth/isLoggedIn"]) {
+			return next({ name: "Home" })
+		}
+
+		return next()
+	}
+
+	return next()
 })
 
 export default router
