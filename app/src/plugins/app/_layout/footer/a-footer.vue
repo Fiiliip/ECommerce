bcf73f357fcfb7830cf678500f13583ea76fda42
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2023-10-24 11:58:37 +0200

    [Footer] Updated footer for better responsivity.

--- app/src/plugins/app/_layout/footer/a-footer.vue
+++ app/src/plugins/app/_layout/footer/a-footer.vue
@@ -1,27 +1,24 @@
 <template>
-    <footer class="w-full flex flex-wrap sm:flex-nowrap gap-8 px-5 py-5 sm:justify-evenly text-sm text-white bg-zinc-900">
-        <div>
-            <h2 class="mb-2 font-medium">O nás</h2>
-            <a class="block" href="#">Čo je ECommerce?</a>
-            <a class="block" href="#">Kontakt</a>
-            <a class="block" href="#">Lorem ipsum dolor sit amet</a>
-            <a class="block" href="#">Lorem ipsum dolor sit amet</a>
+    <footer class="w-full px-5 py-5 grid gap-8 sm:gap-0 sm:grid-cols-2 justify-center text-sm text-white bg-zinc-900">
+        <div class="flex sm:justify-center">
+            <div>
+                <h2 class="mb-2 font-medium">Návody</h2>
+                <a class="block" href="#">Ako nakupovať?</a>
+                <a class="block" href="#">Ako vytvoriť úspešný inzerát?</a>
+            </div>
         </div>
 
-        <div>
-            <h2 class="mb-2 font-medium">Návody</h2>
-            <a class="block" href="#">Ako bezpečne nakupovať?</a>
-            <a class="block" href="#">Ako vytvoriť úspešný inzerát?</a>
-            <a class="block" href="#">Lorem ipsum dolor</a>
-            <a class="block" href="#">Lorem ipsum dolor</a>
-        </div>
-
-        <div>
-            <h2 class="mb-2 font-medium">Nadpis kategórie</h2>
-            <a class="block" href="#">Čo je ECommerce?</a>
-            <a class="block" href="#">Lorem ipsum dolor sit amet</a>
-            <a class="block" href="#">Lorem ipsum dolor sit amet</a>
-            <a class="block" href="#">Lorem ipsum dolor sit amet</a>
+        <div class="flex sm:justify-center">
+            <div class="flex flex-col xs:flex-row">
+                <iframe class="w-36 h-36 rounded" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1462.0595821771276!2d18.73191490429458!3d49.236707389577894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47145c22dda23f15%3A0xbd71b63e641f3630!2sBudat%C3%ADnsky%20park!5e0!3m2!1ssk!2ssk!4v1698139746718!5m2!1ssk!2ssk" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="mt-2 xs:mt-0 xs:ml-2">
+                    <h2 class="mb-2 font-medium">Kontakt</h2>
+                    <p>FRI UNIZA s.r.o.</p>
+                    <p>Topoľová 1,</p>
+                    <p>Žilina-Budatín 010 03</p>
+                    <a class="block" href="mailto:kontakt@fri.uniza.sk">kontakt@fri.uniza.sk</a>
+                </div>
+            </div>
         </div>
     </footer>
 </template>
