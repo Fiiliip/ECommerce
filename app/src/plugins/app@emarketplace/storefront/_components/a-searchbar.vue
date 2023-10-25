63351b1af8ea31571be3e421879aa1010f3a993e
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2023-10-25 23:19:00 +0200

    [Searchbar] Updated searchbar.

--- app/src/plugins/app@emarketplace/storefront/_components/a-searchbar.vue
+++ app/src/plugins/app@emarketplace/storefront/_components/a-searchbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-[95%] sm:w-[70%] flex p-4 bg-white rounded-md shadow-md">
-        <!-- Search Input -->
-        <input type="text" class="w-full p-2 mr-2 border rounded-md focus:ring focus:ring-blue-400" placeholder="Hľadať...">
+        <!-- Search search -->
+        <input v-model="query.search" type="text" class="w-full p-2 mr-2 border rounded-md focus:ring focus:ring-blue-400" placeholder="Hľadať...">
         <!-- Category Dropdown -->
         <div v-if="showCategory" class="relative w-full mr-2">
             <button @click="showDropdown = !showDropdown" class="w-full flex p-2 items-center justify-between border rounded-md hover:bg-zinc-100">
@@ -14,16 +14,16 @@
             <!-- Dropdown -->
             <div :class="[ showDropdown ? 'block' : 'hidden' ]" class="absolute w-full h-64 overflow-y-scroll mt-2 bg-white divide-y divide-zinc-300 rounded-lg shadow z-10">
                 <ul class="py-2 text-sm">
-                    <li v-for="category in categories" :key="category.id">
-                        <a @click="$router.push({ name: 'Category', params: { slug: category.slug} })" :href="`/category/${category.slug}`" class="block px-4 py-2 hover:bg-zinc-100">{{ category.title }}</a>
+                    <li @click="query.category = category.slug" class="cursor-pointer" v-for="category in categories" :key="category.id">
+                        <a @click="search()" :href="query.search ? `/listings/?category=${category.slug}&search=${query.search.replace(' ', '+')}` : `/listings/?category=${category.slug}`" class="block px-4 py-2 hover:bg-zinc-100">{{ category.title }}</a>
                     </li>
                 </ul>
             </div>
         </div>
-        <!-- Search Button -->
-        <button @click="null" class="hidden sm:block w-36 p-2 border rounded-md bg-zinc-900 hover:bg-zinc-700">
-            <img class="w-5 h-5 mx-auto" src="@/plugins/app/_assets/_icons/search.svg">
-        </button>
+        <!-- Search -->
+        <a @click="search()" :href="`${href}`" class="hidden sm:block w-36 p-2 border rounded-md bg-zinc-900 hover:bg-zinc-700">
+            <img class="w-5 h-5 mt-[1px] mx-auto" src="@/plugins/app/_assets/_icons/search.svg">
+        </a>
     </div>
 </template>
 
@@ -39,8 +39,43 @@ export default {
     data() {
         return {
             categories: this.$store.getters['emarketplace/categories'],
+
+            query: {
+                category: '',
+                search: '',
+            },
+            href: '',
+
             showDropdown: false,
         }
     },
+
+    mounted() {
+        this.query.category = this.$route.query.category ? this.$route.query.category : ''
+        this.query.search = this.$route.query.search?.replace('+', ' ')
+    },
+
+    methods: {
+        search() {
+            this.$router.push({ name: 'Listings', query: this.query })
+        },
+    },
+
+    watch: {
+        query: {
+            handler(value) {
+                this.href = '/listings'
+
+                if (value.category && value.search) {
+                    this.href += `/?category=${value.category}&search=${value.search.replace(' ', '+')}`
+                } else if (value.category) {
+                    this.href += `/?category=${value.category}`
+                } else if (value.search) {
+                    this.href += `/?search=${value.search.replace(' ', '+')}`
+                }
+            },
+            deep: true
+        }
+    }
 }
 </script>
