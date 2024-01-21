c812cdb4c4827f895d4bfde08f3e8e9cffd91009
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2024-01-21 23:32:50 +0100

    [Header] Added burger menu and more options.
    - Added burger menu for smaller screens.
    - Added options to view user listings (my listings).

--- app/src/plugins/app/_layout/header/a-header.vue
+++ app/src/plugins/app/_layout/header/a-header.vue
@@ -2,10 +2,32 @@
     <header>
         <div class="w-full h-14 px-5 flex justify-between items-center text-white bg-zinc-900">
             <p @click="$router.push({ name: 'Home' })" class="cursor-pointer">EMarketplace</p>
-            <div class="flex gap-8 text-sm">
+
+            <!-- Larger Screens -->
+            <div class="hidden xs:flex gap-8 text-sm">
                 <a @click="$router.push({ name: 'New Listing '})" class="hover:text-zinc-300" href="/new-listing">Pridať inzerát</a>
-                <a v-if="!this.$store.getters['auth/isLoggedIn']" @click="$router.push({ name: 'Login' })" class="hover:text-zinc-300" href="/auth/login">Prihlásiť sa</a>
-                <a v-else @click="logout()" href="#" class="hover:text-zinc-300">Odhlásiť sa</a>
+                <div v-if="this.$store.getters['auth/isLoggedIn']" class="flex gap-8">
+                    <a @click="$router.push({ name: 'My Listings', query: $store.getters['auth/user'].id})" class="hover:text-zinc-300" :href="`/my-listings/?user=${$store.getters['auth/user'].id}`">Moje inzeráty</a>
+                    <a @click="logout()" href="#" class="hover:text-zinc-300">Odhlásiť sa</a>
+                </div>
+                <a v-else @click="$router.push({ name: 'Login' })" class="hover:text-zinc-300" href="/auth/login">Prihlásiť sa</a>
+            </div>
+
+            <!-- Smaller Screens -->
+            <div class="xs:hidden relative">
+                <button @click="showMobileMenu = !showMobileMenu" class="flex items-center space-y-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+            </div>
+            <div v-if="showMobileMenu" class="xs:hidden absolute top-12 right-0 px-4 pb-2 text-sm bg-zinc-900 rounded-b-lg">
+                <a @click="$router.push({ name: 'New Listing '})" class="block py-2 hover:text-zinc-300" href="/new-listing">Pridať inzerát</a>
+                <div v-if="this.$store.getters['auth/isLoggedIn']" class="flex flex-col gap-2">
+                    <a @click="$router.push({ name: 'My Listings', query: $store.getters['auth/user'].id})" class="hover:text-zinc-300" :href="`/my-listings/?user=${$store.getters['auth/user'].id}`">Moje inzeráty</a>
+                    <a @click="logout()" href="#" class="hover:text-zinc-300">Odhlásiť sa</a>
+                </div>
+                <a v-else @click="$router.push({ name: 'Login' })" class="hover:text-zinc-300" href="/auth/login">Prihlásiť sa</a>
             </div>
         </div>
     </header>
@@ -13,6 +35,12 @@
 
 <script>
 export default {
+    data() {
+        return {
+            showMobileMenu: false,
+        }
+    },
+
     methods: {
         async logout() {
             this.$loader.startLoading('Odhlasujem sa...')
@@ -21,5 +49,11 @@ export default {
         }
     }
 }
-
 </script>
+
+<style lang="sass" scoped>
+
+.user-profile:hover .dropdown
+    @apply block absolute
+    // display: block
+</style>
